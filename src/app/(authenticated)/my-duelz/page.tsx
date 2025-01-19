c325926534765ec5b2d274445d58623d2e8b57df
Shubh <110349596+Crosstons@users.noreply.github.com>
2025-01-19 14:57:01 +0530

    add : basic navigation pages

---
+++ src/app/(authenticated)/my-duelz/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>My-duelz</div>
+  )
+}
+
+export default page
---
+++ src/app/(authenticated)/my-match/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        My-match
+    </div>
+  )
+}
+
+export default page
